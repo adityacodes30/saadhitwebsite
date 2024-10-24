e1c05c19905aba9f54e65c251e85d975e2446604
adityacodes30 <adityaework@gmail.com>
2024-10-24 23:58:51 +0530

    ci cd test2

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
       <section className={stl.heroSection}>
         <div className={stl.heroText}>
           <h1>
-           CI / CD Testing
+           CI / CD Testing 2
           </h1>
         </div>
         <Button text="Get Started" />
