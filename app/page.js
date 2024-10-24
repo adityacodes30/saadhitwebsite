6ba32f7cd6554256742c898e105d3211e893cf74
adityacodes30 <adityaework@gmail.com>
2024-10-24 21:24:45 +0530

    construction

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
       <section className={stl.heroSection}>
         <div className={stl.heroText}>
           <h1>
-           Under construction 6
+           Under construction 7
           </h1>
         </div>
         <Button text="Get Started" />
